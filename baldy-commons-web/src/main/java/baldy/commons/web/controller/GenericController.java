6241438795a29827cad7c463aa627cfc2c67a448
mbmartinez <mbmartinez@exist.com>
2014-01-03 11:28:18 +0800

    moved more stuff over from mtg

---
+++ baldy-commons-web/src/main/java/baldy/commons/web/controller/GenericController.java
@@ -0,0 +1,61 @@
+package baldy.commons.web.controller;
+
+import java.security.Principal;
+
+import org.apache.commons.lang.Validate;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author mbmartinez
+ */
+public abstract class GenericController {
+	
+	protected String name(Principal principal) {
+		return null == principal ? "Anonymous" : principal.getName();
+	}
+
+	protected ModelAndView mav() {
+		return new ModelAndView();
+	}
+	
+	protected ModelAndView mav(String viewname) {
+		return new ModelAndView(viewname);
+	}
+	
+	protected String firstError(BindingResult result) {
+		Validate.isTrue(result.hasErrors());
+        ObjectError error = result.getAllErrors().iterator().next();
+        return error.getDefaultMessage();
+	}
+	
+	protected String basic(String src) {
+		Validate.notNull(src);
+		src = src.trim().replaceAll("[\r\n]+", "br2nl");
+		src = Jsoup.clean(src, Whitelist.basic());
+		return src.replaceAll("br2nl", "\n"); 
+	}
+	
+	protected int getInt(WebRequest request, String paramName) {
+		String strVal = request.getParameter(paramName);
+		Validate.notNull(strVal, paramName + " not found in request!");
+		return Integer.parseInt(strVal);
+	}
+	
+	protected String getString(WebRequest request, String paramName) {
+		return getString(request, paramName, true);
+	}
+	
+    protected String getString(WebRequest request, String paramName, boolean notnull) {
+		String strVal = request.getParameter(paramName);
+		if(notnull) {
+			Validate.notNull(strVal, paramName + " not found in request!");
+		}
+		return strVal;
+	}
+	
+}
---
+++ baldy-commons-web/src/main/java/baldy/commons/web/dto/JSON.java
@@ -0,0 +1,83 @@
+package baldy.commons.web.dto;
+
+import java.util.HashMap;
+
+public class JSON extends HashMap<String, Object> {
+	private static final long serialVersionUID = -8234082507884717784L;
+	
+	/**
+	 * @return status 200 - Ok
+	 */
+	public static JSON ok(String message) {
+		return new JSON().status("200").message(message);
+	}
+	
+	public static JSON ok() {
+		return new JSON().status("200").message("OK");
+	}
+	
+	public static JSON error() {
+		return new JSON().status("500");
+	}
+	
+	public static JSON error(String message) {
+	    return new JSON().status("500").message(message);
+	}
+	
+	/**
+	 * @return status 404 - Not found
+	 */
+	public static JSON status404() {
+		return new JSON().status("404").message("Not found");
+	}
+
+	/**
+	 * @return status 500 - Internal Error
+	 */
+	public static JSON status500(Exception e) {
+		return new JSON().status("500").message(e.getClass() + ": " + e.getMessage());
+	}
+	
+	/**
+	 * @return status 418
+	 */
+	public static JSON teapot() {
+		return new JSON().status("418");
+	}
+	
+	/**
+	 * @return status 409 - Conflict
+	 */
+	public static JSON status409(String... message) {
+		JSON response = new JSON().status("409");
+		if(message.length > 0) response.message(message[0]);
+		return response;
+	}
+	
+	public JSON status(String status) {
+		super.put("status", status);
+		return this;
+	}
+	
+	public JSON message(String message) {
+		super.put("message", message);
+		return this;
+	}
+	
+	public JSON put(String key, Object value) {
+		super.put(key, value);
+		return this;
+	}
+
+	public static JSON translate(OpResult result) {
+		switch(result.getStatus()) {
+		case OK:
+			return JSON.ok(result.getMessage());
+		case ERROR:
+		case FORBIDDEN:
+			return JSON.error(result.getMessage());
+		default:
+			throw new IllegalArgumentException("Unhandled operation result: " + result);
+		}
+	}
+}
